5c6158911b9df3cbe1391d37581a23eea06d4b96
Louis Tiao <louistiao@gmail.com>
2016-02-23 18:09:38 +1100

    getting there

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        super(PyTest, self).initialize_options()
+        self.pytest_args = []
+
+    def finalize_options(self):
+        super(PyTest, self).finalize_options()
+        self.test_suite = True
+        self.test_args = []
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        exit(pytest.main(self.pytest_args))
 
 readme = open('README.rst').read()
 doclink = """
 Documentation
 -------------
 
-The full documentation is at http://{{ cookiecutter.repo_name }}.rtfd.org."""
+The full documentation is at http://nicta.github.io/{{ cookiecutter.repo_name }}/."""
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
@@ -29,26 +38,55 @@ setup(
     author='{{ cookiecutter.full_name }}',
     author_email='{{ cookiecutter.email }}',
     url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    packages=[
-        '{{ cookiecutter.repo_name }}',
-    ],
+    packages=find_packages(),
     package_dir={'{{ cookiecutter.repo_name }}': '{{ cookiecutter.repo_name }}'},
     include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.repo_name }}_cli_script = scripts.{{ cookiecutter.repo_name }}_cli_script:cli'
+        ],
+        'gui_scripts': [
+            '{{ cookiecutter.repo_name }}_gui_script = scripts.{{ cookiecutter.repo_name }}_gui_script:gui'
+        ]
+    },
     install_requires=[
+        'scipy',
+        'numpy',
     ],
-    license='MIT',
+    extras_require={
+        'demos': [
+            'click',
+            'matplotlib'
+        ],
+        'dev': [
+            'sphinx',
+            'ghp-import'
+        ]
+    },
+    cmdclass={
+        'test': PyTest
+    },
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'codecov',
+        'tox',
+    ],
+    license="Apache Software License 2.0",
     zip_safe=False,
     keywords='{{ cookiecutter.repo_name }}',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis"
     ],
 )
